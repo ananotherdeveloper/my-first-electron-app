738a732736d0091659c2e7f7735101b35fd82899
root <root@DESKTOP-QGONNDP.localdomain>
2021-12-29 18:26:16 +0530

    Working for MAC only - added precompiled binary

---
+++ tokenizers/bindings/enums.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PaddingDirection = exports.TruncationStrategy = void 0;
+var TruncationStrategy;
+(function (TruncationStrategy) {
+    TruncationStrategy["LongestFirst"] = "longest_first";
+    TruncationStrategy["OnlyFirst"] = "only_first";
+    TruncationStrategy["OnlySecond"] = "only_second";
+})(TruncationStrategy = exports.TruncationStrategy || (exports.TruncationStrategy = {}));
+var PaddingDirection;
+(function (PaddingDirection) {
+    PaddingDirection["Left"] = "left";
+    PaddingDirection["Right"] = "right";
+})(PaddingDirection = exports.PaddingDirection || (exports.PaddingDirection = {}));
